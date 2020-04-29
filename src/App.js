b37d7c6336a60227c695cc532a8f561cbe39ba3d
Gal Gur <gal.gurevich@gmail.com>
2020-04-29 19:49:09 +0300

    added state, generic structure, example of use using dummy data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Select from './myselect/Select'
 
 function App() {
+
+  const languages = [
+    { value: 'en', text: 'English' },
+    { value: 'he', text: 'עברית' },
+    { value: 'fr', text: 'français'}
+  ];
+
+  const [language, setLanguage] = useState('en');
+
   return (
     <div className="App">
-     <Select></Select>
-     <div className="txt">YOU CHOOSE OPTION: </div>
+     <Select options={languages} value={language} onChange={value => setLanguage(value)}></Select>
+     <div className="txt">YOU CHOOSE OPTION: {language}</div>
     </div>
   );
 }
--- src/myselect/Select.js
+++ src/myselect/Select.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Select.css'
 
 
-export default function Select() {
+export default function Select({options, value, onChange}) {
 
-    function selectedOption() {
-        alert("some msg");
+    const [selectedValue, setSelectedValue] = useState(value);
+
+    const selectedOption = options.find(option => option.value === selectedValue);
+
+    function updateValue(val) {
+        onChange(val);
+        setSelectedValue(val);
     }
 
     return(
     <div className="wrapper">
             <div className="toggle">
-                Choose an option:
+                {selectedOption.text}
                 <div className="arrow"/>
             </div>
 
             <div className="dropdown">
-                <div className="option" onClick={selectedOption}>Option 1!</div>
-                <div className="option" onClick={selectedOption}>Option 2!</div>
+            {options.map((option) => <div className="option" onClick={() => updateValue(option.value)}>{option.text}</div>)}
             </div> 
         </div>
 )}
